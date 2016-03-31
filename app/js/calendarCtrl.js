3b08ee380d3dc39d41a7dda782dcb816f3d91810
Pruek <pk_lk@hotmail.com>
2016-03-31 14:18:50 +0200

    tweak calendar

--- app/js/calendarCtrl.js
+++ app/js/calendarCtrl.js
@@ -10,7 +10,7 @@ meetingPlannerApp.controller('CalendarCtrl', function ($scope, Agenda) {
   	};
 
   	$scope.inlineOptions = {
-    	customClass: getDayClass,
+    	// customClass: getDayClass,
     	minDate: new Date(),
     	showWeeks: true
   	};
@@ -44,13 +44,13 @@ meetingPlannerApp.controller('CalendarCtrl', function ($scope, Agenda) {
    //  	$scope.popup2.opened = true;
   	// };
 
-  	$scope.setDate = function(year, month, day) {
-    	$scope.dt = new Date(year, month, day);
-  	};
+ //  	$scope.setDate = function(year, month, day) {
+ //    	$scope.dt = new Date(year, month, day);
+ //  	};
 
-	$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-  	$scope.format = $scope.formats[0];
-  	$scope.altInputFormats = ['M!/d!/yyyy'];
+	// $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+ //  	$scope.format = $scope.formats[0];
+ //  	$scope.altInputFormats = ['M!/d!/yyyy'];
 
   	// $scope.popup1 = {
    //  	opened: false
@@ -60,37 +60,37 @@ meetingPlannerApp.controller('CalendarCtrl', function ($scope, Agenda) {
    //  	opened: false
   	// };
 
-  	var tomorrow = new Date();
-  	tomorrow.setDate(tomorrow.getDate() + 1);
-  	var afterTomorrow = new Date();
-  	afterTomorrow.setDate(tomorrow.getDate() + 1);
-  	$scope.events = [
-    	{
-      		date: tomorrow,
-      		status: 'full'
-    	},
-    	{
-      		date: afterTomorrow,
-      		status: 'partially'
-    	}
-  	];
+ //  	var tomorrow = new Date();
+ //  	tomorrow.setDate(tomorrow.getDate() + 1);
+ //  	var afterTomorrow = new Date();
+ //  	afterTomorrow.setDate(tomorrow.getDate() + 1);
+ //  	$scope.events = [
+ //    	{
+ //      		date: tomorrow,
+ //      		status: 'full'
+ //    	},
+ //    	{
+ //      		date: afterTomorrow,
+ //      		status: 'partially'
+ //    	}
+ //  	];
 
-  	function getDayClass(data) {
-    	var date = data.date,
-      	mode = data.mode;
-    	if (mode === 'day') {
-	      	var dayToCheck = new Date(date).setHours(0,0,0,0);
+ //  	function getDayClass(data) {
+ //    	var date = data.date,
+ //      	mode = data.mode;
+ //    	if (mode === 'day') {
+	//       	var dayToCheck = new Date(date).setHours(0,0,0,0);
 
-	      	for (var i = 0; i < $scope.events.length; i++) {
-	        	var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+	//       	for (var i = 0; i < $scope.events.length; i++) {
+	//         	var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
 
-	        	if (dayToCheck === currentDay) {
-	          		return $scope.events[i].status;
-	        	}
-	      	}
-    	}
-    	return '';
-	}
+	//         	if (dayToCheck === currentDay) {
+	//           		return $scope.events[i].status;
+	//         	}
+	//       	}
+ //    	}
+ //    	return '';
+	// }
 
 
 });
