029bc1d29be75df7d6f2c9bd2287ea04993e0f52
engslatt <engslatt@kth.se>
2016-04-10 16:57:56 +0200

    new drag-n-drop frida

--- app/js/activitiesCtrl.js
+++ app/js/activitiesCtrl.js
@@ -22,13 +22,6 @@ meetingPlannerApp.controller('ActivitiesCtrl', function ($scope,$firebaseArray,$
 
 	}
 	
-	//$scope.drag = function(event){
-	//	ev.dataTransfer.setData("text", ev.target.id);
-	//}
-
-	document.ondragstart = function(ev){
-	 	ev.dataTransfer.setData("text", ev.target.id);
-	}
 
 
 	$scope.openAddActivity = function () {
@@ -37,6 +30,26 @@ meetingPlannerApp.controller('ActivitiesCtrl', function ($scope,$firebaseArray,$
 	      controller: 'ActivityModalCtrl'
     	});
   	};
+
+  	$scope.centerAnchor = true;
+        $scope.toggleCenterAnchor = function () {$scope.centerAnchor = !$scope.centerAnchor}
+        //$scope.draggableObjects = [{name:'one'}, {name:'two'}, {name:'three'}];
+        $scope.droppedObjects1 = [];
+        $scope.onDropComplete1=function(data,evt){
+            var index = $scope.droppedObjects1.indexOf(data);
+            if (index == -1)
+            $scope.droppedObjects1.push(data);
+        }
+        $scope.onDragSuccess1=function(data,evt){
+            console.log("133","$scope","onDragSuccess1", "", evt);
+            var index = $scope.droppedObjects1.indexOf(data);
+            if (index > -1) {
+                $scope.droppedObjects1.splice(index, 1);
+            }
+        }
+        var inArray = function(array, obj) {
+            var index = array.indexOf(obj);
+        }
 
 });
 
--- app/js/overviewCtrl.js
+++ app/js/overviewCtrl.js
@@ -15,28 +15,25 @@ meetingPlannerApp.controller('OverviewCtrl', function ($scope,Agenda, $firebaseO
     	});
   	};
 
-  	//$scope.allowDrop = function(ev){
-	//	ev.preventDefault();
-	//}
-
-	// $scope.drop = function(){
-	// 	ev.preventDefault();
-	//     var data = ev.dataTransfer.getData("text");
-	//     ev.target.appendChild(document.getElementById(data));
-	//     Agenda.deleteAct(data);
-	// }
-
-	document.ondragover = function(ev) {
-	     ev.preventDefault();
-	}
-
-	document.ondrop = function(ev){
-		ev.preventDefault();
-	    var data = ev.dataTransfer.getData("text");
-	    ev.target.appendChild(document.getElementById(data));
-	    Agenda.deleteAct(data);
-	   
-	}
+	$scope.centerAnchor = true;
+        $scope.toggleCenterAnchor = function () {$scope.centerAnchor = !$scope.centerAnchor}
+        //$scope.draggableObjects = [{name:'one'}, {name:'two'}, {name:'three'}];
+        $scope.droppedObjects1 = [];
+        $scope.onDropComplete1=function(data,evt){
+            var index = $scope.droppedObjects1.indexOf(data);
+            if (index == -1)
+            $scope.droppedObjects1.push(data);
+        }
+        $scope.onDragSuccess1=function(data,evt){
+            console.log("133","$scope","onDragSuccess1", "", evt);
+            var index = $scope.droppedObjects1.indexOf(data);
+            if (index > -1) {
+                $scope.droppedObjects1.splice(index, 1);
+            }
+        }
+        var inArray = function(array, obj) {
+            var index = array.indexOf(obj);
+        }
 });
 
 
